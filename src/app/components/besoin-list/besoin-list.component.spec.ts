26025adf10aafea80d6d0d3f44a18d0445b92e78
MonsterH-H <handyjeux@gmail.com>
2025-05-16 01:45:58 +0100

    Résolution des conflits

--- src/app/components/besoin-list/besoin-list.component.spec.ts
+++ src/app/components/besoin-list/besoin-list.component.spec.ts
@@ -1,23 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { BesoinListComponent } from './besoin-list.component';
 
 describe('BesoinListComponent', () => {
   let component: BesoinListComponent;
   let fixture: ComponentFixture<BesoinListComponent>;
-
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [BesoinListComponent]
-    })
-    .compileComponents();
+    
+  })
 
-    fixture = TestBed.createComponent(BesoinListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
--- src/app/services/farmer.service.spec.ts
+++ src/app/services/farmer.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { FarmerService } from './farmer.service';
 
@@ -6,7 +7,10 @@ describe('FarmerService', () => {
   let service: FarmerService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [FarmerService]
+    });
     service = TestBed.inject(FarmerService);
   });
 
